892d4d5fa1aca0201d12a46ac0809648c276fe52
Kaen Pawanachai <pawaox4th@gmail.com>
2023-02-28 15:20:58 +0700

    Update output message to include arrow indicator for clarity.

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,5 @@ import { Elysia } from "elysia"
 const app = new Elysia().get("/", () => "Hello Elysia").listen(3310)
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+  `🦊 Elysia is running at >> ${app.server?.hostname}:${app.server?.port}`
 )
